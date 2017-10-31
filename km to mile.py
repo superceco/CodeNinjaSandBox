8451647f8533a4f5fae13d0b1d49fe0fa9496c98
superceco <superceco@gmx.at>
2017-10-31 18:12:24 +0100

    Yo raps

--- km to mile.py
+++ km to mile.py
@@ -1,9 +1,13 @@
+print("Hello Human my name is MileKill and I will convert "
+      "killometers into miles for you")
+
 while True:
     try:
         kilometer = float(input("Enter the kilometers: "))
-        miles= 1/1.6
-        print( kilometer ,"km" ,"are ", kilometer*miles, "miles")
+        miles = 1 / 1.6093440
+        print(kilometer, "km", "are ", kilometer * miles, "miles")
         break
     except:
         print("Enter correct value")
         continue
+
