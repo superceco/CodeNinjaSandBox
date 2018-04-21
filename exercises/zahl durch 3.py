482fdf86e0d1395931a1721ae7cfd4ba04f51798
superceco <superceco@gmx.at>
2018-04-21 13:24:08 +0200

    Delete zahl durch 3.py

--- exercises/zahl durch 3.py
+++
@@ -1,9 +0,0 @@
-b = int(input("Enter the highest number: "))
-sum = 0
-for i in range(1,b):
-    if i%3 == 0:
-        print(i)
-        sum = sum + 1
-    else:
-        continue
-print("total number of numbers is:", sum)
